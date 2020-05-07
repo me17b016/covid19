f9cb5485debb67faf3e0782f0c4e970fda44d8e3
me17b016 <rajgarg383@gmail.com>
2020-05-07 17:34:10 +0530

    Updates

---
+++ precache-manifest.c84756714290dc733176964da4272a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa22526264e7a923a4e7351aeaa3d5ea",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "f832c648391ed02ce209",
+    "url": "/covid19/static/css/main.4be7b620.chunk.css"
+  },
+  {
+    "revision": "6d410207abe2b0da9459",
+    "url": "/covid19/static/js/2.52242f0a.chunk.js"
+  },
+  {
+    "revision": "0b14dac90b918c082faed0d9d1548202",
+    "url": "/covid19/static/js/2.52242f0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f832c648391ed02ce209",
+    "url": "/covid19/static/js/main.06d4f333.chunk.js"
+  },
+  {
+    "revision": "080958ad2f3d2ab682fd",
+    "url": "/covid19/static/js/runtime-main.6dbe199c.js"
+  }
+]);
