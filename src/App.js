63db3cd5ba913747e627aeca636866a97ec338fe
me17b016 <rajgarg383@gmail.com>
2020-05-07 18:08:49 +0530

    removed console logs

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ class App extends Component {
       }
       districtsData = {...districtsData, [districtUniqueKey] : dataObject}
     }
-    console.log(districtsData)
     this.setState(() => ({ districts, districtsData }))
   }
 
